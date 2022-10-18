cc6d0b836e4794690192757ef89bef73ca4adf0a
Sukhchain Singh <sukh5395@gmail.com>
2022-10-18 17:37:38 +0530

    Changes

--- example/example.dart
+++ example/example.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
-import 'package:dart_spider/spider.dart';
+import 'package:dart_spider/dart_spider.dart';
 import 'package:dart_spider/src/models/config_model.dart';
 import 'package:dart_spider/src/models/optional_model.dart';
 import 'package:dart_spider/src/models/parser_model.dart';
